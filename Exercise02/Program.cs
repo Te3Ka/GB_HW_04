197e9e8f6ca21cc459a7d50d5e133c28e305d41d
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-01-25 21:29:01 +0300

    Задание 2. Получение числа

--- Exercise02/Program.cs
+++ Exercise02/Program.cs
@@ -17,9 +17,25 @@ void Author()
     Console.WriteLine("E-mail: Mnement4813@yandex.ru");
 }
 
+int Absolute (int num)
+{
+    if (num < 0)
+    {
+        num *= (-1);
+        return num;
+    }
+    else
+        return num;
+}
+
 /*
 Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 452 -> 11
 82 -> 10
 9012 -> 12
 */
+
+Console.WriteLine("Программа считает сумму цифр в введённом числе.");
+Console.Write("Введите число: ");
+int.TryParse(Console.ReadLine(), out int number);
+number = Absolute(number);
